dbf39c962a48e75dd29de3084d37eed45b40d343
yuu ogasawara <y.ogasawara@st-ventures.jp>
2017-03-16 17:33:15 +0900

    Fixed.#3の課題が完了しました。

---
+++ 1-1-2/1-1-2/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  1-1-2
+//
+//  Created by yogasawara@stv on 2017/03/16.
+//  Copyright © 2017年 yogasawara@stv. All rights reserved.
+//
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController{
+    NSArray *array;
+    NSDictionary *dictionary;
+}
+
+
+@end
+
