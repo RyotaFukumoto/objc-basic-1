735f0440900f5c6d9422124d4b39ea042d2a4534
yuu ogasawara <y.ogasawara@st-ventures.jp>
2017-03-20 00:07:51 +0900

    Fixed. #14の課題が完了しました。

---
+++ 2-1-1/2-1-1/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  2-1-1
+//
+//  Created by yogasawara@stv on 2017/03/19.
+//  Copyright © 2017年 smart tech ventures. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController{
+    __weak IBOutlet UILabel *label;
+}
+
+
+@end
+
---
+++ 2-1-1/2-1-1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  2-1-1
+//
+//  Created by yogasawara@stv on 2017/03/19.
+//  Copyright © 2017年 smart tech ventures. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
