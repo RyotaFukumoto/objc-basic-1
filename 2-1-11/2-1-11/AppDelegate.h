d45704aeab7c4c4604722a6d75ae9865341e5524
Yuu Ogasawara <y.ogasawara@st-ventures.jp>
2017-03-23 18:54:43 +0900

    TableViewに画像と文字列を表示する

---
+++ 2-1-11/2-1-11/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  2-1-11
+//
+//  Created by yuu ogasawara on 2017/03/23.
+//  Copyright © 2017年 stv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 2-1-11/2-1-11/ViewController.h
@@ -0,0 +1,18 @@
+//
+//  ViewController.h
+//  2-1-11
+//
+//  Created by yuu ogasawara on 2017/03/23.
+//  Copyright © 2017年 stv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property NSArray *devices;
+
+@end
+
+@interface ViewController (TableViewDelegate)<UITableViewDataSource,UITableViewDelegate>
+
+@end
