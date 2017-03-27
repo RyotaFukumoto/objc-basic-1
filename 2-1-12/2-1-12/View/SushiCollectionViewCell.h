c15587a00f25a3c4c38f090ec1765354b8e1282e
yuu ogasawara <y.ogasawara@st-ventures.jp>
2017-03-27 16:01:39 +0900

    Fixed. #39の課題が完了しました。
    UICollectionViewでセクションなし、一列に画像を表示する

---
+++ 2-1-12/2-1-12/View/SushiCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  SushiCollectionViewCell.h
+//  PlayUICollectionView
+//
+//  Created by yogasawara@stv on 2017/03/27.
+//  Copyright © 2017年 smart tech ventures. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SushiCollectionViewCell : UICollectionViewCell
+@property UIImageView *sushiImageView;
+@end
