0818bb2987f7d205a754f530297d32bae54acad4
yuu ogasawara <y.ogasawara@st-ventures.jp>
2017-03-28 19:20:32 +0900

    TabBarで二つの画面を実装
    各画面のアイコン、画像を設定。

---
+++ 2-1-15/2-1-15/ViewController/OsakaViewController.h
@@ -0,0 +1,13 @@
+//
+//  OsakaViewController.h
+//  2-1-15
+//
+//  Created by yogasawara@stv on 2017/03/28.
+//  Copyright © 2017年 smart tech ventures. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OsakaViewController : UIViewController
+
+@end
---
+++ 2-1-15/2-1-15/ViewController/OsakaViewController.m
@@ -0,0 +1,37 @@
+//
+//  OsakaViewController.m
+//  2-1-15
+//
+//  Created by yogasawara@stv on 2017/03/28.
+//  Copyright © 2017年 smart tech ventures. All rights reserved.
+//
+
+#import "OsakaViewController.h"
+
+@interface OsakaViewController ()
+
+@end
+
+@implementation OsakaViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
