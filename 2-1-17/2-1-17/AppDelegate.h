86e4912445d6710a2811a6bb7655ef5d5ac4f565
Yuu Ogasawara <y.ogasawara@st-ventures.jp>
2017-03-30 10:55:07 +0900

    Fiexd. #52の課題が完了しました。

---
+++ 2-1-17/2-1-17/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  2-1-17
+//
+//  Created by yuu ogasawara on 2017/03/30.
+//  Copyright © 2017年 stv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
