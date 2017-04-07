d4a1389476be1709d5356474d28e53ca5e27d7c0
Yuu Ogasawara <y.ogasawara@st-ventures.jp>
2017-04-07 18:16:52 +0900

    Fixed. #79の課題が完了しました。
    AFNetworkingのインストール。

---
+++ 2-4-1/2-4-1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  2-4-1
+//
+//  Created by yuu ogasawara on 2017/04/07.
+//  Copyright © 2017年 stv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
