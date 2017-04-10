e46a734c02e74131dc55a6135bddc2d86170ffcc
yuu ogasawara <y.ogasawara@st-ventures.jp>
2017-04-10 10:34:37 +0900

    天気予報APIから情報を取得
    今日明日の天気予報を配列に格納。ATS対応済み。

--- 2-4-1/2-4-1/ViewController.m
+++ 2-4-1/2-4-1/ViewController.m
@@ -12,14 +12,33 @@
 NSString* const kWeatherReportAPIURLForTokyo = @"http://weather.livedoor.com/forecast/webservice/json/v1?city=130010";
 
 @interface ViewController ()
-
+@property NSArray<NSDictionary*>* forecastsArray;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSString *url = kWeatherReportAPIURLForTokyo;
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    
+    [manager GET:url parameters:nil progress:nil
+         success:^(NSURLSessionTask *task, id responseObject) {
+             // json取得に成功した場合の処理
+             //responseObjectはNSDictionary型
+             NSDictionary *dict = (NSDictionary*)responseObject;
+             
+             //今日明日明後日の情報を配列に格納する
+             self.forecastsArray = dict[@"forecasts"];
+             DLog(@"%@",self.forecastsArray.description);
+         }
+     
+         failure:^(NSURLSessionTask *operation, NSError *error) {
+             // エラーの場合の処理
+             NSLog(@"error");
+         }
+     ];
 }
 
 
