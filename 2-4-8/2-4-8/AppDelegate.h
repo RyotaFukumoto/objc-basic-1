168f06c7ef8787978e48c62e2ee3b02fb4894973
Yuu Ogasawara <y.ogasawara@st-ventures.jp>
2017-04-17 17:11:52 +0900

    Fixed. #91の課題が完了しました。
    Instagramへの投稿を、instagram公式アプリに画像を渡すことで行う。

---
+++ 2-4-8/2-4-8/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  2-4-8
+//
+//  Created by yuu ogasawara on 2017/04/17.
+//  Copyright © 2017年 stv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 2-4-8/2-4-8/ViewController.m
@@ -0,0 +1,44 @@
+//
+//  ViewController.m
+//  2-4-8
+//
+//  Created by yuu ogasawara on 2017/04/17.
+//  Copyright © 2017年 stv. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "PostInstagram.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)viewDidAppear:(BOOL)animated{
+    // ViewContollerからの呼び出しを想定
+    if ([PostInstagram canInstagramAppOpen]) {
+        
+        UIImage *image = [UIImage imageNamed:@"cookie"]; // <- 投稿したい画像を準備しておく
+        
+        PostInstagram *instagramViewController = [[PostInstagram alloc] init];
+        [instagramViewController setImage:image];
+        [self.view addSubview:instagramViewController.view];
+        [self addChildViewController:instagramViewController];
+        
+    } else {
+        // Instagramがインストールされていない
+    }
+}
+@end
