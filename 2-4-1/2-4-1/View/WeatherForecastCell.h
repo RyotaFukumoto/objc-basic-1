d3ba9fd4a98a7b217a18fbab670a6df98cdb1d63
Yuu Ogasawara <y.ogasawara@st-ventures.jp>
2017-04-17 17:58:49 +0900

    セルの再表示前に、キャッシュを消去

--- 2-4-1/2-4-1/View/WeatherForecastCell.h
+++ 2-4-1/2-4-1/View/WeatherForecastCell.h
@@ -14,7 +14,10 @@
 @property (weak, nonatomic) IBOutlet UILabel *dateLabel;
 @property (weak, nonatomic) IBOutlet UILabel *weatherLabel;
 @property (weak, nonatomic) IBOutlet UIImageView *weatherImageView;
+
 -(void)setCellFor:(WeatherForecast*)weatherForecast;
 -(void)configureCellFor:(WeatherRecord*)weatherRecord;
+
 + (NSString*)className;
+
 @end
