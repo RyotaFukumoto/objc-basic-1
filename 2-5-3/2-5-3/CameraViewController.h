9f129f626b60543547b01590ae2e5bcad7c61e54
Yuu Ogasawara <y.ogasawara@st-ventures.jp>
2017-04-18 14:06:56 +0900

    plistの編集
    カメラ撮影、画像ライブラリへのアクセスに必要な設定を追加。

---
+++ 2-5-3/2-5-3/CameraViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  2-5-3
+//
+//  Created by yuu ogasawara on 2017/04/18.
+//  Copyright © 2017年 stv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CameraViewController : UIViewController
+
+
+@end
---
+++ 2-5-3/2-5-3/CameraViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  2-5-3
+//
+//  Created by yuu ogasawara on 2017/04/18.
+//  Copyright © 2017年 stv. All rights reserved.
+//
+
+#import "CameraViewController.h"
+
+@interface CameraViewController ()
+
+@end
+
+@implementation CameraViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
