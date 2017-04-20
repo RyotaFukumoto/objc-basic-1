fae6681baf3195748ae228196c5be2277b11269b
yuu ogasawara <y.ogasawara@st-ventures.jp>
2017-04-20 16:43:56 +0900

    Fixed. #106 の課題が完了しました。

---
+++ 2-5-6/2-5-6/ViewController.h
@@ -0,0 +1,18 @@
+//
+//  ViewController.h
+//  2-5-6
+//
+//  Created by yogasawara@stv on 2017/04/20.
+//  Copyright © 2017年 smart tech ventures. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "CoreLocation/CoreLocation.h"
+
+//参考 http://blog.koogawa.com/entry/2016/03/26/181906
+//参考　http://blog.koogawa.com/entry/2014/08/12/191215
+@interface ViewController : UIViewController<CLLocationManagerDelegate>
+
+
+@end
+
