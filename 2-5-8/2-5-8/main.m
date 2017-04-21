d34ee3e599aa5e074e21d1bfa1f5412f454c3d65
Yuu Ogasawara <y.ogasawara@st-ventures.jp>
2017-04-21 11:11:26 +0900

    Cocoapodsでのfirebaseの導入、capabilityの設定

---
+++ 2-5-8/2-5-8/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  2-5-8
+//
+//  Created by yuu ogasawara on 2017/04/21.
+//  Copyright © 2017年 stv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
